b2b13ab3257f39bc220eafc03a7b24385bae1dd0
Paulo <106405308+Paulo19961944@users.noreply.github.com>
2023-08-13 12:42:16 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,25 +6,21 @@ let banco = document.querySelector("#inputConta");
 let btn = document.querySelector("#submit");
 btn.addEventListener("click", capturarTeclado)
 
-/** Função para capturar teclados */
+/** Função para capturar teclado */
 function capturarTeclado() {
     console.log("O seu nome é: " + nome.value + " O seu email é: " + email.value + " A sua senha é: " + password.value + " A sua conta bancária é: " + banco.value)
 }
 
 /** Função Teclado */
 let textoDigitado = "";
-
 function teclaPressionada(event) {
     const codigoTecla = event.keyCode || event.which;
-
     /** Ignorar as teclas Shift e Caps Lock */
     if (codigoTecla === 16 || codigoTecla === 20) {
         return;
     }
     const caractere = String.fromCharCode(codigoTecla);
-
     if (codigoTecla === 8) {
-
         /** Apaga caracteres */
         textoDigitado = textoDigitado.slice(0, -1);
     } else if (codigoTecla === 13) {
@@ -34,10 +30,8 @@ function teclaPressionada(event) {
         /** Tratamento das teclas */
         textoDigitado += caractere;
     }
-
     console.clear();
     console.log(textoDigitado);
 }
-
 /** Adiciona o Teclado */
 document.addEventListener("keydown", teclaPressionada);
